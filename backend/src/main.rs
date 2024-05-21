f068aa62d66bb8d459f562b084876453b371dbce
Dennis Kobert <dennis@kobert.dev>
2024-05-21 17:54:53 +0200

    Make backend server single threaded and sync

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,61 +1,149 @@
 mod protocol;
 
-use std::{collections::HashMap, sync::Arc, time::Duration};
+use std::{
+    collections::{hash_map::Entry, HashMap},
+    io::{ErrorKind, Read},
+    net::{SocketAddr, TcpListener, TcpStream},
+    str::FromStr,
+};
 
-use protocol::ProtocolStream;
-use tokio::sync::Mutex;
-use tokio::{net::TcpListener, spawn, time::timeout};
+use crate::protocol::Error;
 
-use crate::protocol::Protocol;
-
-#[derive(Default)]
+#[derive(Default, Debug)]
 struct UserAuth {
     users: HashMap<String, String>,
 }
 
 impl UserAuth {
-    pub async fn is_valid_or_insert(&mut self, username: String, password: String) -> bool {
-        *self.users.entry(username).or_insert(password.clone()) == password
+    pub fn is_valid_or_insert(&mut self, username: String, password: String) -> Option<String> {
+        match self.users.entry(username) {
+            Entry::Vacant(v) => {
+                let key = v.key().clone();
+                v.insert(password);
+                Some(key)
+            }
+            Entry::Occupied(o) => (o.get() == &password).then(|| o.key().clone()),
+        }
+    }
+}
+type Color = u32;
+type Position = (u32, u32);
+
+#[derive(Debug, Default, Clone)]
+struct Board;
+
+impl Board {
+    fn place(&mut self, x: u32, y: u32, addr: SocketAddr) {
+        todo!()
     }
 }
 
-#[tokio::main]
-async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:1312").await?;
-    let user_auth = Arc::new(Mutex::new(UserAuth::default()));
+#[derive(Debug)]
+struct Connection {
+    addr: SocketAddr,
+    username: Option<String>,
+    color: Color,
+    stream: TcpStream,
+    next_stone: Option<Position>,
+}
+
+#[derive(Default, Debug)]
+struct GameState {
+    users: Vec<Connection>,
+    user_auth: UserAuth,
+    board: Board,
+}
+
+enum Command {
+    Login(String, String),
+    Put(Position),
+}
+impl FromStr for Command {
+    type Err = Error;
+
+    fn from_str(line: &str) -> Result<Self, Self::Err> {
+        let mut args = line.splitn(2, '\n');
+        let args = std::array::from_fn(|_| args.next().unwrap_or_default());
+        match args {
+            ["LOGIN", user, password] => Ok(Command::Login(user.to_owned(), password.to_owned())),
+            ["PUT", x, y] => {
+                let x: u32 = x.parse().map_err(|_| Error::InvalidArgument)?;
+                let y: u32 = y.parse().map_err(|_| Error::InvalidArgument)?;
+                Ok(Command::Put((x, y)))
+            }
+            _ => Err(Error::UnknownCommand),
+        }
+    }
+}
+
+fn parse_line(stream: &mut TcpStream, parse: impl Fn(&str) -> Result<Command, Error>) -> Result<Command, Error> {
+    let mut buf = [0; 1024];
+    let bytes = stream.peek(&mut buf)?;
+    let pos = buf[0..bytes]
+        .iter()
+        .position(|a| a == &b'\n')
+        .ok_or(Error::WouldBlock)?;
+    stream.read_exact(&mut buf[0..pos])?;
+    let str = std::str::from_utf8(&buf[0..pos])?;
+    parse(str)
+}
+
+impl GameState {
+    fn process_user_input(&mut self) {
+        for user in self.users.iter_mut() {
+            loop {
+                match parse_line(&mut user.stream, Command::from_str) {
+                    Ok(Command::Login(username, password)) => {
+                        if let Some(username) = self.user_auth.is_valid_or_insert(username, password) {
+                            user.username = Some(username);
+                        } else {
+                            eprintln!("Invalid Credentials");
+                        }
+                    }
+                    Ok(Command::Put((x, y))) => self.board.place(x, y, user.addr),
+                    Err(Error::WouldBlock) => break,
+                    Err(error) => eprintln!("error while reading user input: {error}"),
+                }
+            }
+        }
+    }
+}
+
+fn random_color() -> Color {
+    todo!()
+}
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:1312")?;
+    listener.set_nonblocking(true)?;
+    let mut game = GameState::default();
 
     loop {
-        let (stream, addr) = listener.accept().await?;
-        let mut stream = ProtocolStream::new(stream);
-
-        println!("New connection {addr}");
-
-        let users = user_auth.clone();
-
-        spawn(async move {
-            let task = async { handshake(users, &mut stream).await };
-            let elapsed = timeout(Duration::from_secs(3), task).await;
-
-            let err = match elapsed {
-                Ok(Err(e)) => stream.write(&Protocol::Error(e)).await,
-                Err(_) => stream.write(&Protocol::Error(protocol::Error::Timeout)).await,
-                _ => return,
-            }
-            .unwrap_err();
-
-            eprintln!("Could not write to stream: {err}. Killing connection");
-        });
+        if let Err(e) = accept_new_connections(&listener, &mut game) {
+            eprintln!("Error while accepting a new connection: {e}");
+        }
     }
 }
 
-async fn handshake(users: Arc<Mutex<UserAuth>>, stream: &mut ProtocolStream) -> Result<(), protocol::Error> {
-    proto_expect!(stream, Protocol::Login(username, password));
-    println!("{username} has password {password}");
-
-    if !users.lock().await.is_valid_or_insert(username, password).await {
-        return Err(protocol::Error::InvalidCredentials);
+fn accept_new_connections(listener: &TcpListener, game: &mut GameState) -> std::io::Result<()> {
+    loop {
+        match listener.accept() {
+            Ok((stream, addr)) => {
+                stream.set_nonblocking(true)?;
+                let con = Connection {
+                    addr,
+                    username: None,
+                    color: random_color(),
+                    stream,
+                    next_stone: None,
+                };
+                game.users.push(con);
+            }
+            Err(ref e) if e.kind() == ErrorKind::WouldBlock => {
+                break;
+            }
+            Err(e) => eprintln!("socket error: {e}"),
+        }
     }
-
-    stream.write(&Protocol::Motd("Hey".into())).await?;
     Ok(())
 }
--- backend/src/protocol.rs
+++ backend/src/protocol.rs
@@ -1,113 +1,28 @@
 use std::{fmt::Display, marker::PhantomData};
 
-use tokio::io::{AsyncBufReadExt, AsyncWriteExt};
-use tokio::{io::BufStream, net::TcpStream};
-
-pub trait Serialize {
-    fn serialize(&self) -> Result<String, Error>;
-}
-
-pub trait Deserialize
-where
-    Self: Sized,
-{
-    fn deserialize(input: &str) -> Result<Self, Error>;
-}
-
 #[derive(Debug)]
 pub enum Error {
-    UnexpectedEof,
     InvalidArgument,
     UnknownCommand,
-    UnexpectedCommand,
-    Timeout,
     InvalidCredentials,
+    WouldBlock,
+    IO(std::io::Error),
+    Utf8(std::str::Utf8Error),
 }
 
-pub enum Protocol {
-    Login(String, String),
-    Put(u32, u32),
-    Error(Error),
-    Motd(String),
+impl From<std::io::Error> for Error {
+    fn from(value: std::io::Error) -> Self {
+        Error::IO(value)
+    }
+}
+impl From<std::str::Utf8Error> for Error {
+    fn from(value: std::str::Utf8Error) -> Self {
+        Error::Utf8(value)
+    }
 }
 
 impl Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{self:?}")
     }
-}
-
-impl Protocol {
-    fn serialize(&self) -> Result<String, Error> {
-        Ok(match self {
-            Protocol::Login(username, password) => format!("LOGIN {username} {password}"),
-            Protocol::Put(x, y) => format!("PUT {x} {y}"),
-            Protocol::Error(e) => format!("ERROR {e}"),
-            Protocol::Motd(motd) => format!("MOTD {motd}"),
-        })
-    }
-
-    fn deserialize(input: &str) -> Result<Self, Error> {
-        let mut input = input.split(' ');
-
-        macro_rules! parse {
-            () => {{
-                input
-                    .next()
-                    .ok_or(Error::UnexpectedEof)
-                    .and_then(|val| val.parse().map_err(|_| Error::InvalidArgument))
-            }};
-        }
-
-        Ok(match input.next().unwrap() {
-            "LOGIN" => Self::Login(parse!()?, parse!()?),
-            "PUT" => Self::Put(parse!()?, parse!()?),
-            _ => Err(Error::UnknownCommand)?,
-        })
-    }
-}
-
-pub struct ProtocolStream {
-    stream: BufStream<TcpStream>,
-}
-
-impl ProtocolStream {
-    pub fn new(stream: TcpStream) -> Self {
-        Self {
-            stream: BufStream::new(stream),
-        }
-    }
-
-    pub async fn write(&mut self, what: &Protocol) -> Result<(), Error> {
-        self.stream
-            .write_all(what.serialize()?.as_bytes())
-            .await
-            .expect("Could not write to stream, fuck you");
-
-        let _ = self.stream.write_u8(b'\n').await;
-
-        self.stream.flush().await.expect("Could not flush, fuck you");
-
-        Ok(())
-    }
-
-    pub async fn read(&mut self) -> Result<Protocol, Error> {
-        let mut buf = String::new();
-
-        self.stream
-            .read_line(&mut buf)
-            .await
-            .expect("Could not read line, also fix this");
-
-        Protocol::deserialize(&buf)
-    }
-}
-
-#[macro_export]
-macro_rules! proto_expect {
-    ($stream: expr, $e: pat) => {
-        let $e = $stream.read().await? else {
-            return Err(protocol::Error::UnexpectedCommand);
-        };
-    };
 }
