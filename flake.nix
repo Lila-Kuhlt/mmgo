eb4c122761ec33ef1b271f5f904e5a4a415a992b
Dennis Kobert <dennis@kobert.dev>
2024-05-21 15:11:02 +0200

    Reorder folder structure

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        toolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = ["rust-src" "clippy" "rust-analyzer"];
+        };
+
+
+        libraries = with pkgs;[
+          openssl_3
+        ];
+
+        packages = with pkgs; [
+          pkg-config
+          dbus
+          openssl_3
+          bacon
+
+          bun
+          toolchain
+        ];
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = packages;
+
+          shellHook =
+            ''
+              export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
+            '';
+        };
+      });
+}
+
